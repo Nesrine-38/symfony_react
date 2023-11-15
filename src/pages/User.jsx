6ef61ffda541693869c5c0badcd2a658b4ff72dc
nesrine ishak <nesrineissac@gmail.com>
2023-11-15 20:04:38 +0100

    liste_utilisateurs

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import './User.css'
+import React, { useState, useEffect } from 'react';
 
 const User = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // obtenir la liste des utilisateurs par le lien /api/users
+    fetch('/api/users')
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
+  }, []);
+  
   return (
     <div>
-      
+      <h2>Liste des utilisateurs</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.nom} {user.prenom} - {user.age} ans
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default User
